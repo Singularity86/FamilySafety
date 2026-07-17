cea5fbf0c372b849e316c56f5859fbae48af27f3
Singularity86 <omar@courageonpurpose.org>
2026-07-17 16:07:46 -0700

    Bump version to 1.11.1 (13) for permission-copy and tutorial fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         applicationId = "jibaro.spacepirate.love"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.11.0"
+        versionCode = 13
+        versionName = "1.11.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
